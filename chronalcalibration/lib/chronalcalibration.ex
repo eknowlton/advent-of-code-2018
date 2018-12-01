f72074d9da48120144979898e8c509636f6574dc
Ethan Knowlton <eknowlton@gmail.com>
2018-12-01 17:59:15 -0500

    closer to calibration

--- chronalcalibration/lib/chronalcalibration.ex
+++ chronalcalibration/lib/chronalcalibration.ex
@@ -1,26 +1,13 @@
 defmodule ChronalCalibration do
-  @moduledoc """
-  Documentation for ChronalCalibration.
-  """
+  def calibrate(in_list) do
+    in_list
+    |> Enum.reduce(fn x, acc -> acc + x end)
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ChronalCalibration.hello()
-      :world
-
-  """
-  def read_input
+  def parse_input do
     File.stream!('input.txt')
     |> Stream.map(&String.trim/1)
     |> Stream.map(&Integer.parse/1)
     |> Enum.to_list
-  end
-
-  def hello(in_list) do
-    in_list
-    |> Enum.reduce(fn x, acc -> acc + x end)
   end
 end
--- chronalcalibration/test/chronalcalibration_test.exs
+++ chronalcalibration/test/chronalcalibration_test.exs
@@ -2,7 +2,7 @@ defmodule ChronalCalibrationTest do
   use ExUnit.Case
   doctest ChronalCalibration
 
-  test "sums the list" do
-    assert [1, -2, 3] |> ChronalCalibration.hello() == 2
+  test "calibrates time" do
+    assert [1, -2, 3] |> ChronalCalibration.calibrate() == 2
   end
 end
