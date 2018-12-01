b0d0f841a944532c089c632849ef8dc66b0c2919
Ethan Knowlton <eknowlton@gmail.com>
2018-12-01 18:58:26 -0500

    trying to use gaurds to make recursive functions

--- chronalcalibration/lib/chronalcalibration.ex
+++ chronalcalibration/lib/chronalcalibration.ex
@@ -1,7 +1,15 @@
 defmodule ChronalCalibration do
-  def calibrate(in_list) do
-    in_list
-    |> Enum.sum()
+  def calibrate(in_list, now, frequencies) when now in frequencies do
+    now
+  end
+
+  def calibrate((head | tail), now, frequencies) do
+    calibrate(tail, now + head, [now + head] + frequencies)
+  end
+
+  def calibrate([], now, frequencies) do
+    parse_input()
+    |> calibrate(frequencies)
   end
 
   def parse_input do
